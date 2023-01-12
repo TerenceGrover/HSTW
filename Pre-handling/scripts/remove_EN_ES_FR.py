e31e14978cf5ff8765fc03b80c1552ba9df5c2ff
TerenceGrover <terencegrover@gmail.com>
2023-01-12 15:51:19 +0100

    Implemented translate, sentiment analysis and most common word

--- Pre-handling/scripts/remove_EN_ES_FR.py
+++ Pre-handling/scripts/remove_EN_ES_FR.py
@@ -3,13 +3,13 @@ import json
 english_txt = open('../data/LANG/english_sources.txt')
 lines = english_txt.read().split(' \n')
 french_txt = open('../data/LANG/french_sources.txt')
-lines = french_txt.read().split(' \n')
+lines += french_txt.read().split(' \n')
 spanish_txt = open('../data/LANG/spanish_sources.txt')
 lines += spanish_txt.read().split(' \n')
 
 print(lines)
 
-with open('../../server/data/10-01-23.json') as parsed:
+with open('../../server/data/TODAY/11-01-23.json') as parsed:
   parsedloaded = json.load(parsed)
   zib = 0
   zob = 0
--- server/scripts/RSS_explorer.py
+++ server/scripts/RSS_explorer.py
@@ -1,11 +1,16 @@
+import os
+from dotenv import load_dotenv
 import feedparser
 import boto3
+import pandas as pd
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 import json
+import string
 from datetime import datetime, date, timedelta
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-import os
-from dotenv import load_dotenv
 
 load_dotenv()
 DB_STRING = os.getenv('DB_STRING')
@@ -17,85 +22,172 @@ server_api=ServerApi('1'))
 db = client.test
 collection = db.test_collection
 
-scraped = False
-translated = False
+now = datetime.now()
+time1 = now.strftime('%H:%M')
+today = now.strftime('%d-%m-%y')
+
+sources_EN = {'en': ['AS', 'AG', 'AU', 'BS', 'BB', 'BZ', 'BM', 'DM', 'FO', 'FJ', 'GM', 'GH', 'GI', 'GD', 'GY', 'IN', 'IE', 'IM', 'JM', 'JE', 'LS', 'LR', 'MW', 'MT', 'MH', 'MU', 'NZ', 'SH', 'WS', 'SC', 'SB', 'ZA', 'KN', 'LC', 'VC', 'TC', 'GB', 'AE', 'US', 'VI', 'ZM']}
+sources_FR = {'fr':['DZ', 'BE', 'BJ', 'TD', 'CD', 'FR', 'GF', 'PF', 'GA', 'GP', 'HT', 'ML', 'YT', 'NC', 'NE', 'CG', 'RE', 'SN', 'TG']}
+sources_ES = {'es':['AD', 'AR', 'BO', 'CL', 'CR', 'CU', 'DO', 'EC', 'SV', 'GT', 'HN', 'PA', 'PE', 'PR', 'ES', 'UY', 'VE']}
+
+sources = sources_EN['en'] + sources_FR['fr'] + sources_ES['es']
 
 def scrapeSources():
 
-  global scraped
+  global time1
+  global today
 
-  dict = {}
-  now = datetime.now()
-  time1 = now.strftime('%H:%M')
+  # Read the JSON file into a dataframe
+  df = pd.read_json('../data/sourceLinks.json', typ='series')
+
+  # Initialize an empty dictionary to store the results
   char_counter = 0
-  date = now.strftime('%d-%m-%y')
+
+  # Iterate over the rows of the dataframe
+  for country, links in df.items():
+    print(country)
+    headlines = []
+    
+    # Iterate over the links for each country
+    for link in links.values():
+        try:
+            feed = feedparser.parse(link)
+        except:
+            continue
+
+        title_counter = 0
+        hl_counter = 0
+        titles = []
+      
+        # Use list comprehension to select the entries that meet the conditions
+        try:
+          for entry in feed.entries:
+            if 5 <= len(entry.title) <= 60 and title_counter <= 4 and len(headlines) <= 10:
+                titles.append(entry.title)
+                char_counter += len(entry.title)
+                title_counter += len(titles)
+        except:
+          continue
+
+        # Use list extend to add the titles to the headlines list
+        headlines.extend(titles)
+        hl_counter += len(titles)
+        
+    # Call the processor with headlines and country as args
+    processor(headlines, country)
+
+  # Print the time and character count
+  newNow = datetime.now()
+  print(f'Time 1 : {time1}')
+  print(newNow.strftime('Time 2 : %H:%M'))
+  print(f'Characters {char_counter}')
+  print(f'HeadLines {hl_counter}')
+
+####### HELPER FUNCTIONS TO CHECK SENTENCES ##########
+
+english_stopwords = set(stopwords.words("english"))
+
+def is_ascii(word):
+
+  return all(ord(c) < 128 for c in word)
+
+def is_english(sentence):
+
+  if not is_ascii(sentence):
+    return False
+  words = word_tokenize(sentence)
+  words = [word.lower() for word in words if word.isalpha()]
+  words = [word for word in words if word not in english_stopwords]
+  return len(words) > 0
+
+def removeStopWords(sentence):
+
+    words = word_tokenize(sentence)
+    # Remove stopwords from the list of words
+    words = [word for word in words if word.lower() not in english_stopwords]
+    # Join the remaining words back into a sentence
+    return " ".join(words)
+
+#####################################################
+
+def processor(headlines, country):
+
+  global today
+  global sources
+
+  if country not in sources:
+    trans = translateHL(headlines,country)
+  else:
+    trans = headlines
 
   try:
-    with open('../data/sourceLinks.json') as parsed:
-      json_obj = json.load(parsed)
+  # Open the JSON file
+    with open(f"../data/{today}test.json", 'r') as file:
+      writable = json.loads(file.read())
+    file.close()
 
-      for elem in json_obj.items():
-        dict[elem[0]] = []
-        for link in elem[1].values():
-          try:
-            feed = feedparser.parse(link)
-            topHeadlines = []
-          except:
-            continue
+  except FileNotFoundError:
+      print(f"Error: The file '{today}test.json' does not exist.")
+      writable = {}
 
-          try:
-            for entry in feed.entries:
-              if len(topHeadlines) > 2:
-                break
-              if len(entry.title) > 60 or len(entry.title) < 5:
-                continue
-              else : 
-                char_counter = char_counter + len(entry.title)
-                topHeadlines.append(entry.title)
-                dict[elem[0]].append(entry.title)
-          except:
-            continue
+  except json.decoder.JSONDecodeError:
+      print(f"Error: The file '{today}test.json' is not in json format.")
+      writable = {}
 
-        with open(f"../data/{date}.json", "w") as outfile:
-          json.dump(dict, outfile, ensure_ascii=False)
+  country_obj = {
+    'idx' : sentimentHL(trans, country),
+    'topics' : most_common_words(trans),
+    'HL' : trans
+  }
 
-      newNow = datetime.now()
+  writable[country] = country_obj
+  print(writable[country])
 
-      print(f'Time 1 : {time1}')
-      print(newNow.strftime('Time 2 : %H:%M'))
-      print(f'Characters {char_counter}')
+  with open(f"../data/{today}.json", 'w', encoding='utf8') as file:
+    json.dump(writable, file, ensure_ascii=False)
 
-      scraped = True
+def translateHL(headlines, country):
+
+  global today
+  global sources
+
+  if country not in sources:
+    translated_texts = []
+    for sentence in headlines:
+      if not is_english(sentence):
+
+        client = boto3.client(
+          'translate',
+          region_name='eu-west-1',
+          aws_access_key_id=ACCESS_KEY,
+          aws_secret_access_key=SECRET_KEY)
+
+        try:
+          response = client.translate_text(
+            Text = sentence,
+            SourceLanguageCode = 'auto',
+            TargetLanguageCode = 'en'
+          )
+          
+          translated_texts.append(response['TranslatedText'])
+
+        except Exception as e:
+          print(e)
+
+      else: 
+        translated_texts.append(sentence)
+
+    return translated_texts
+
+def sentimentHL(translatedHL, country):
+
+  global sources
+
+  try:
+    str = " ".join(translatedHL)
   except:
     print(Exception)
-
-def translateHL():
-  processedJSON = open("../data/TODAY/processed.json", "w")
-  json.dump(dict, processedJSON, ensure_ascii=False)
-
-  today_HL = {}
-
-  client = boto3.client(
-    'translate',
-    region_name='eu-west-1',
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_KEY,)
-
-  try:
-    response = client.translate_text(
-      Text = HLcoll,
-      SourceLanguageCode = 'auto',
-      TargetLanguageCode = 'en'
-    )
-    return response['TranslatedText']
-  except Exception as e:
-    print(e)
-
-def sentimentHL():
-  # processedJSON = open("../data/TODAY/processed.json", "w")
-  # json.dump(dict, processedJSON, ensure_ascii=False)
-
-  today_HL = {}
+    pass
 
   client = boto3.client(
     'comprehend',
@@ -103,17 +195,52 @@ def sentimentHL():
     aws_access_key_id=ACCESS_KEY,
     aws_secret_access_key=SECRET_KEY,)
 
-  try:
-    response = client.detect_sentiment(
-      Text = "I love you but you're toxic",
-      LanguageCode = 'en'
-    )
-    print(response['SentimentScore'])
-  except Exception as e:
-    print(e)
+  if country in sources_ES:
+    try:
+      response = client.detect_sentiment(
+        Text = str,
+        LanguageCode = 'es'
+      )
+    except Exception as e:
+      print(e)
 
-def createIndex():
-  print('idx')
+  elif country in sources_FR:
+    try:
+      response = client.detect_sentiment(
+        Text = str,
+        LanguageCode = 'fr'
+      )
+    except Exception as e:
+      print(e)
+
+  else:
+    str = removeStopWords(str)
+    try:
+      response = client.detect_sentiment(
+        Text = str,
+        LanguageCode = 'en'
+      )
+    except Exception as e:
+      print(e)
+  
+  idx = {
+    'P' : response['SentimentScore']['Positive'],
+    'N' : response['SentimentScore']['Negative'],
+    'Nu' : response['SentimentScore']['Neutral'],
+    'M' : response['SentimentScore']['Mixed']
+  }
+
+  return idx
+
+def most_common_words(headlines):
+  joined = " ".join(headlines.strip(string.punctuation))
+  noStopword = removeStopWords(joined)
+  tokens = word_tokenize(noStopword)
+  fdist = nltk.FreqDist(tokens)
+  most_common_words = [word for word, freq in fdist.items() if freq >= 2]
+  most_common_words = fdist.most_common(5)
+
+  return most_common_words
 
 def cleaner():
   yesterday = date.today() - timedelta(days=1)
@@ -127,20 +254,11 @@ def cleaner():
   except:
     pass
 
-def streamLine():
 
-  global scraped
-  global translated
 
-  scrapeSources()
+scrapeSources()
 
-  if scraped:
-    translateHL()
 
-  if translated:
-    createIndex()
-
-cleaner()
 
 # CHECKER FOR INDIVIDUAL LINKS FOR MANUAL GATHERING
 
