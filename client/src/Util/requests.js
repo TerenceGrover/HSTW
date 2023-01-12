9a55d93ce27d2685e34f75177767c3a504ba685a
TerenceGrover <terencegrover@gmail.com>
2023-01-12 20:41:12 +0100

    Added request services on front

--- client/src/Util/requests.js
+++ client/src/Util/requests.js
@@ -1,4 +1,4 @@
-const url = 'http://127.0.0.1:5000'
+const url = 'localhost:5001'
 
 // IMPORITNG THE SETTER FUNCTION AS AN ARGUMENT ALLOWS US
 // TO SKIP AN AWAIT ALL TOGETHER
@@ -9,9 +9,27 @@ const url = 'http://127.0.0.1:5000'
 //   .then(data => setter(data))
 // }
 
+export function getTodayIndividualData(alphaCode) {
+  return fetch(`${url}/today?code=${alphaCode}`)
+    .then(response => response.json())
+    .then(data => JSON.parse(data))
+}
+
+export function getDateSpecificIndividualData(alphaCode, date) {
+  return fetch(`${url}/request?code=${alphaCode}&date=${date}`)
+    .then(response => response.json())
+    .then(data => JSON.parse(data))
+}
+
+export function getWorldToday() {
+  return fetch(`${url}/today?code=world`)
+    .then(response => response.json())
+    .then(data => JSON.parse(data))
+}
+
 export function getCountryDetails(alphaCode) {
   return fetch(`https://restcountries.com/v3.1/alpha/${alphaCode}?fields=name,flag,capital,currencies,languages,region,capital,demonyms`)
-  .then(response => response.text())
-  .then(data => JSON.parse(data))
-  .catch(err => err)
+    .then(response => response.text())
+    .then(data => JSON.parse(data))
+    .catch(err => err)
 }
