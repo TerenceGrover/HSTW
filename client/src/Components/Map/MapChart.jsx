7815f4bf130477787fde3e8ec4f5f8b55b9cd3c0
TerenceGrover <terencegrover@gmail.com>
2023-01-14 17:59:47 +0100

    Auto-rotate and more styling

--- client/src/Components/Map/MapChart.jsx
+++ client/src/Components/Map/MapChart.jsx
@@ -7,6 +7,8 @@ const geoUrl = process.env.PUBLIC_URL + '/assets/Topology.json';
 
 export default function MapChart({ clickSet, clicked }) {
 
+  const globeEl = useRef();
+
   const [mobile, setMobile] = useState(false);
   const [idx, setIdx] = useState(false);
   const [countries, setCountries] = useState({ features: []});
@@ -70,6 +72,10 @@ export default function MapChart({ clickSet, clicked }) {
       .then(countries=> {
         setCountries(countries);
       });
+    globeEl.current.controls().autoRotate = true;
+    globeEl.current.controls().autoRotateSpeed = 0.3;
+
+    globeEl.current.pointOfView({ altitude: 2 }, 3000);
 
     return () => {
       window.removeEventListener('resize', handleWindowSizeChange);
@@ -83,6 +89,7 @@ export default function MapChart({ clickSet, clicked }) {
       ?
       <div id="mobile-container">
       <Globe 
+      ref = {globeEl}
       height={500}
       width={window.innerWidth - 24}
       globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
@@ -94,12 +101,15 @@ export default function MapChart({ clickSet, clicked }) {
       onPolygonClick={d => clickSet({name : d.properties.NAME, 'Alpha-2' : d.properties.ISO_A2})}
       polygonStrokeColor={() => '#111'}
       polygonLabel={({ properties: d }) => `${d.ADMIN} | ${d.ISO_A2}`}
+      polygonAltitude={0}
+      polygonsTransitionDuration={1000}
       />
       </div>
       :
       <div id="map-container">
       <Globe 
-      height={600}
+      ref = {globeEl}
+      height={window.innerHeight / 1.5}
       width={window.innerWidth - 40}
       globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
       backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
