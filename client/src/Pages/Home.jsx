b5f0b9dcea3e610bc2af8f8c3656fafdae6fedb6
rakesh <rakeshhotker@gmail.com>
2023-02-15 18:36:35 +0530

    fixed Improvement on the About Page#8

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -9,12 +9,13 @@ export default function Home({mobile, innerWidth, idx, clicked, setClicked}) {
 
   const scrollToRef = useRef();
   const [loader, setLoader] = useState(true);
-
+  
 
   // eslint-disable-next-line no-restricted-globals
   // screen.orientation.lock('portrait')
 
   useEffect(() => {
+    document.querySelector("body").scrollTo(0,0)	  
     setTimeout(() => {
       setLoader(false);
     }, 1250);
